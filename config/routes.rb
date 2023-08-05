aebf433a5cbb16e8a179b7ee25c64f0315f7e4c1
mion1213 <141290890+mion1213@users.noreply.github.com>
2023-08-05 15:20:06 +0000

    [Finish] 4-7

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'homes/index'
   get 'homes/show'
   get 'homes/edit'
+  get 'homes/about' => 'homes#about' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
